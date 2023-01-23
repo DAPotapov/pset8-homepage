1aa98117cf8bfd08f1b4aa36f8fb9f8778513ddc
DAPotapov <103317598+DAPotapov@users.noreply.github.com>
2023-01-23 13:25:12 +0200

    Credit card form finished. Some improvements in logic

--- files/services.js
+++ files/services.js
@@ -48,60 +48,61 @@ function validation(){
 
 
     var numberInput = document.getElementById("card_number-input").value;
-    // Lets control that user don't input anything besides numbers and space (it's ok - I'll get rid of it later)
+    // Lets control that user don't input anything besides numbers and spaces (spaces ok - they will be cleaned later)
     var pattern = /[^ 0-9]/;
     // It's true if there are anything except digits and space
     if (pattern.test(numberInput)){
         document.getElementById('nonvalid').removeAttribute('hidden');
-        cardlogo.removeChild(cardlogo.lastChild);
     }
+    else{
+    // If no garbage in string then proceed with determine of payment system and validation
+            // Clear string from spaces
+        numberInput = numberInput.replace(/\s+/g, '');
+        var n = numberInput.length;
 
-        // Clear string from spaces
-    numberInput = numberInput.replace(/\s+/g, '');
-    var n = numberInput.length;
-
-    // Once 2 digits of number is written start checking which bank system card belongs
-    if (n > 1) {
-        var text = "Payment system: ";
-        var textnode = document.createTextNode('');
-        if (numberInput.startsWith('2')) {
-            textnode.nodeValue = 'MIR';
+        // Once 2 digits of number is written start checking which bank system card belongs
+        if (n > 1) {
+            var text = "Payment system: ";
+            var textnode = document.createTextNode('');
+            if (numberInput.startsWith('2')) {
+                textnode.nodeValue = 'MIR';
+            }
+            if (numberInput.startsWith('4')) {
+                textnode.nodeValue = 'VISA';
+            }
+            if (/^3[47]/.test(numberInput)) {
+                textnode.nodeValue = 'AMEX';
+            }
+            if (/^5[1-5]/.test(numberInput)) {
+                textnode.nodeValue = 'MASTERCARD';
+            }
+            // Add cardname only if it has one
+            if (textnode.nodeValue != '')
+            {
+                textnode.nodeValue = text + textnode.nodeValue;
+                var cardname = document.createElement('p');
+                cardname.appendChild(textnode);
+                cardname.setAttribute('id', 'banksystem');
+                document.getElementById("cardlogo").appendChild(cardname);
+            }
         }
-        if (numberInput.startsWith('4')) {
-            textnode.nodeValue = 'VISA';
-        }
-        if (/^3[47]/.test(numberInput)) {
-            textnode.nodeValue = 'AMEX';
-        }
-        if (/^5[1-5]/.test(numberInput)) {
-            textnode.nodeValue = 'MASTERCARD';
-        }
-        // Add cardname only if it has one
-        if (textnode.nodeValue != '')
-        {
-            textnode.nodeValue = text + textnode.nodeValue;
-            var cardname = document.createElement('p');
-            cardname.appendChild(textnode);
-            cardname.setAttribute('class', 'banksystem');
-            document.getElementById("cardlogo").appendChild(cardname);
-        }
-    }
 
 
-    // When it is long enough lets validate it
-    if (n > 12) {
-        // Assume that card number max length is limited 16 digits. We can scale it later if needed.
-        var zeros = '0';
-        for (let i = 1; i < 16 - n; i++){
-            zeros = zeros + '0';
-        }
-        var cardnumber = zeros + numberInput;
-        if (validate(cardnumber)){
-             // button buy
-            document.getElementById("buybtn").removeAttribute('disabled', '');
-        }
-        else{
-            document.getElementById("buybtn").setAttribute('disabled', '');
+        // When it is long enough lets validate it
+        if (n > 12) {
+            // Assume that card number max length is limited 16 digits. We can scale it later if needed.
+            var zeros = '0';
+            for (let i = 1; i < 16 - n; i++){
+                zeros = zeros + '0';
+            }
+            var cardnumber = zeros + numberInput;
+            if (validate(cardnumber)){
+                // button buy
+                document.getElementById("buybtn").removeAttribute('disabled', '');
+            }
+            else{
+                document.getElementById("buybtn").setAttribute('disabled', '');
+            }
         }
     }
 }
